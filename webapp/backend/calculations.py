c2e3f66dba81e0cb125fef62266e632389d1b255
Shiroiii <dima.akarawak@gmail.com>
2023-05-13 22:41:48 +0100

    fixed the distance calculation and added loading more data.

--- webapp/backend/calculations.py
+++ webapp/backend/calculations.py
@@ -2,7 +2,8 @@ from get_data import Datasets
 import geopandas as gpd
 from shapely.geometry import Point
 import folium
-import pandas as pd   
+import pandas as pd
+import pyproj
 
 data = Datasets()
 
@@ -17,12 +18,18 @@ def distance_calculator(longitude, latitude, n_closest):
     # Creating a shapely point geometry for the given location
     location = Point(longitude, latitude)
 
+    # I converted the crs of the input
+    # Create a transformer object to transform from EPSG:4326 to EPSG:32645
+    transformer = pyproj.Transformer.from_crs("EPSG:4326", "EPSG:32645")
+
+    location = Point(transformer.transform(location.x, location.y))
+
     # The power plants locations come in two different datasets with no direct field to concat on.
     # This first half calculates the distance between the given location and the plants on the first dataset and creates a dataframe to hold the information
     distance = list(data.power_plants.geometry.distance(location))
     plant_distance = pd.DataFrame({'power_plant': list(data.power_plants['description']), 'distance': distance})
 
-    # Repeats the process for the second dataframe then concats the two dataframes
+    # Repeats the process for the second dataframe then concat the two dataframes
     distance_2 = list(power_plants_2.geometry.distance(location))
     plant_distance_2 = pd.DataFrame({'power_plant': list(power_plants_2['PLANT']), 'distance': distance_2})
     plant_distance = pd.concat([plant_distance, plant_distance_2])
@@ -40,5 +47,12 @@ def distance_calculator(longitude, latitude, n_closest):
     return plant_distance.head(n_closest), transmission_substations_distance.head(n_closest)
 
 
-print(distance_calculator(7.5000,5.20000,5))
+# print(distance_calculator(7.5000, 5.20000, 5))
 
+
+def calculate_score() -> float:
+    """
+    This function calculates the score for a particular region. This score is an indicator of the potential of a region
+    as a good place for solar power intervention.
+    """
+    pass
--- webapp/backend/get_data.py
+++ webapp/backend/get_data.py
@@ -1,4 +1,5 @@
 import geopandas as gpd
+import pandas as pd
 
 
 class Datasets:
@@ -23,27 +24,97 @@ class Datasets:
         self.grid = gpd.read_file('../../data/modelled_grid_original.csv',
                             GEOM_POSSIBLE_NAMES='geom',
                             KEEP_GEOM_COLUMNS='NO')
-        
+
+        self.irradiance = self.get_ghi_data()
+
+        self.electricity = self.get_electricity_data()
+
         # self.convert_to_crs_types()
     
-    def convert_to_crs_types(self) -> None:
-        # power_plants
-        self.power_plants.set_crs("EPSG:3857", inplace=True)
-        self.power_plants.to_crs("EPSG:4326", inplace=True)
-        self.power_plants = self.power_plants.dropna()
+    # def convert_to_crs_types(self) -> None:
+    #     # power_plants
+    #     self.power_plants.set_crs("EPSG:3857", inplace=True)
+    #     self.power_plants.to_crs("EPSG:4326", inplace=True)
+    #     self.power_plants = self.power_plants.dropna()
+    #
+    #     # power_plants_2
+    #     self.power_plants_2.set_crs('EPSG:4326',inplace=True)
+    #
+    #     # state_boundaries
+    #     self.state_boundaries.set_crs("EPSG:3857",inplace=True)
+    #     self.state_boundaries.to_crs("EPSG:4326",inplace=True)
+    #     self.state_boundaries['center'] = self.state_boundaries.geometry.centroid
+    #
+    #     # transmission_substations
+    #     self.transmission_substations.set_crs("EPSG:3857",inplace=True)
+    #     self.transmission_substations.to_crs("EPSG:4326",inplace=True)
+    #
+    #     # grid
+    #     self.grid.set_crs("EPSG:3857",inplace=True)
+    #     self.grid.to_crs("EPSG:4326",inplace=True)
 
-        # power_plants_2
-        self.power_plants_2.set_crs('EPSG:4326',inplace=True)
+    def get_ghi_data(self):
 
-        # state_boundaries
-        self.state_boundaries.set_crs("EPSG:3857",inplace=True)
-        self.state_boundaries.to_crs("EPSG:4326",inplace=True)
-        self.state_boundaries['center'] = self.state_boundaries.geometry.centroid
+        solcast_index = pd.read_csv("../../data/Solcast/Solcast/solcast_index.csv")
 
-        # transmission_substations
-        self.transmission_substations.set_crs("EPSG:3857",inplace=True)
-        self.transmission_substations.to_crs("EPSG:4326",inplace=True)
+        state_boundaries = gpd.read_file('../../data/nigeria_state_boundaries.csv', GEOM_POSSIBLE_NAMES='geom',
+                                         KEEP_GEOM_COLUMNS='NO')
 
-        # grid
-        self.grid.set_crs("EPSG:3857",inplace=True)
-        self.grid.to_crs("EPSG:4326",inplace=True)
+        # literally cut and join. Will be revised later.
+
+        # Applies the function across all states dataset and appends the result to the empty list
+        irradiance_list = []
+        with open('./links.txt', 'r') as links:
+            filepaths = links.read().split('\n')
+        for index, link in enumerate(filepaths):
+            state = solcast_index.loc[index, 'State']
+            irradiance_list.append(self._get_irradiance(state, link.replace("/content/drive/MyDrive/TID_Innovation/Data", "../../data/Solcast")))
+
+        # Creates_DataFrame
+        Ghi = [item[0] for item in irradiance_list]
+        GtiFixedTilt = [item[1] for item in irradiance_list]
+        GtiTracking = [item[2] for item in irradiance_list]
+        state = list(solcast_index.State)
+        df_dict = {'state': state, 'ghi': Ghi, 'gtifixed': GtiFixedTilt, 'gtitracking': GtiTracking}
+        avg_daily_irradiance_2022 = pd.DataFrame(df_dict)
+
+        # Merges the irradiance data to the state_map Data
+        state_irradiance = state_boundaries.merge(avg_daily_irradiance_2022, left_on='adm1_en', right_on='state')
+
+        return state_irradiance
+
+    @staticmethod
+    def _get_irradiance(state, csv_path):
+        """
+            This function reads in, cleans and calculates the relevant data from given files
+        """
+        # Reading_csv
+        state = pd.read_csv(csv_path)
+        # Retrieving_the_date_from_the_PeriodStart_column
+        state[['date','start_time']]=state['PeriodStart'].str.split('T',expand=True)
+        # Converting the hourly irradiance data to daily irradiance data(Wh/day/m2)                                          
+        daily_irradiance=state.groupby('date').sum()[['Ghi', 'GtiFixedTilt', 'GtiTracking']]
+        # calculates and returns the average daily irradiance data for the year 2022. It also converts it from W to KW                               
+        return list(round(daily_irradiance.mean() / 1000, 2))
+
+    @staticmethod
+    def get_electricity_data():
+        file = "../../data/Nigeria Electricity Data.xlsx"
+        df_2021 = pd.read_excel(file, sheet_name="2021 MIS").set_index("State").drop(columns=["Country", "Survey"])
+        df_2018 = pd.read_excel(file, sheet_name="2018 DHS").set_index("State").drop(columns=["Country", "Survey"])
+        df_2015 = pd.read_excel(file, sheet_name="2015 MIS").set_index("State").drop(columns=["Country", "Survey"])
+        df_2013 = pd.read_excel(file, sheet_name="2013 DHS").set_index("State").drop(columns=["Country", "Survey"])
+
+        # drop 'household with electricity column for all dataframes
+        pop_2021 = df_2021.drop(columns="Households with electricity")
+        pop_2018 = df_2018.drop(columns="Households with electricity")
+        pop_2015 = df_2015.drop(columns="Households with electricity")
+        pop_2013 = df_2013.drop(columns="Households with electricity")
+
+        # merge the dataframes into one
+        pop_df = pd.merge(pop_2013, pop_2015, on='State', suffixes=(' (2013)', ' (2015)'))
+        pop_df = pd.merge(pop_df, pop_2018, on='State')
+        pop_df = pd.merge(pop_df, pop_2021, on='State', suffixes=(' (2018)', ' (2021)'))
+        pop_df.columns = ["2013", "2015", "2018", "2021"]
+
+        return pop_df
