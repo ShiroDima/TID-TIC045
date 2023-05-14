18286e5ac76266b05086622d5c437521c715a962
A.J Edwin <dezwhy97@gmail.com>
2023-05-14 20:31:32 +0100

    added locations dropdown to searchbar

---
+++ docs/main.js
@@ -0,0 +1,48 @@
+
+var locations = 
+[
+   "Abuja",
+   "Abia",
+   "Adamawa",
+   "Akwa Ibom",
+   "Anambra",
+   "Bauchi",
+   "Bayelsa",
+   "Benue",
+   "Borno",
+   "Cross River",
+   "Delta",
+   "Ebonyi",
+   "Edo",
+   "Ekiti",
+   "Enugu",
+   "Gombe",
+   "Imo",
+   "Jigawa",
+   "Kaduna",
+   "Kano",
+   "Katsina",
+   "Kebbi",
+   "Kogi",
+   "Kwara",
+   "Lagos",
+   "Nassarawa",
+   "Niger",
+   "Ogun",
+   "Ondo",
+   "Osun",
+   "Oyo",
+   "Plateau",
+   "Rivers",
+   "Sokoto",
+   "Taraba",
+   "Yobe",
+   "Zamfara"
+];
+var datalist = document.getElementById('locations');
+
+locations.forEach(function(item){
+   var option = document.createElement('option');
+   option.value = item;
+   datalist.appendChild(option);
+});
